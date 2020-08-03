eec08d6faaeecd4d77afc99e6ef32ccfd5e2ca56
sol <sol@DESKTOP-0J7JOH0>
2020-08-03 15:54:14 +0900

    commit3

---
+++ springTest/src/main/java/com/pangzo0/www/springTest/Greeter.java
@@ -0,0 +1,17 @@
+package com.pangzo0.www.springTest;
+
+public class Greeter {
+	private String format;
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	
+	
+
+}

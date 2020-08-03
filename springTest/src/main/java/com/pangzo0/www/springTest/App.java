354147fc1753e585784de850e9acb3c244b2878f
sol <sol@DESKTOP-0J7JOH0>
2020-08-03 15:02:18 +0900

    commit1

---
+++ springTest/src/main/java/com/pangzo0/www/springTest/App.java
@@ -0,0 +1,13 @@
+package com.pangzo0.www.springTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
